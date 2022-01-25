7198b0c5c1d60644775c086a19bb08caab4d1eab
MarieCHessler <marie@hessler.se>
2022-01-25 14:55:46 +0000

    Wire up APIs and confirm access to Google sheet

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import of gspread library and Credentials class from Google auth library
+and wireup APIS, based on instructions from CI's Love Sandwiches project
+"""
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('design_your_own_totebag')  # Access Google sheet
+
+design = SHEET.worksheet('design')
+
+data = design.get_all_values()  # Create a list of list
+
+print(data)
