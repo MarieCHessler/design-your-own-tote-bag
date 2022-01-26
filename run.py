2bab9d028242e0e316036b88d3385a57bb4cb541
MarieCHessler <marie@hessler.se>
2022-01-26 10:10:36 +0000

    After Gitpod crash, insert new creds file, inport gspread library and Google auth library Credentials class, and test function

--- run.py
+++ run.py
@@ -14,19 +14,17 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('design_your_own_totebag')  # Access Google sheet
+SHEET = GSPREAD_CLIENT.open('design_your_own_tote_bag')  # Access Google sheet
 
 
 def intro():
-    """
-    Welcome and information on how to design your own bag
-    """
-    print(" ______       _ __            ___ ")                 
-    print("(  /  _/_    ( /  )          ( / \ ")       
-    print("  /__ /  _    /--< __, _,     /  /_  (  o  _,  __ ") 
-    print("_/(_)(__(/_  /__ /(_(_(_)_  (/\_/(/_/_)_(_(_)_/ /_ ")
-    print("                       /|                  /|")       
-    print("                      (/                  (/ \n\n")    
-    print("Welcome to Tote Bag Design!" 
-          "Here you can create a custom design for your tote bag")
+    """ Welcome and information on how to design your own bag """
 
+    print(" ______       _ __            ___ ")
+    print("(  /  _/_    ( /  )          ( / \ ")
+    print("  /__ /  _    /--< __, _,     /  /_  (  o  _,  __ ")
+    print("_/(_)(__(/_  /__ /(_(_(_)_  (/\_/(/_/_)_(_(_)_/ /_ ")
+    print("                       /|                  /|")
+    print("                      (/                  (/ \n\n")
+    print("Welcome to Tote Bag Design!"
+          "Here you can create a custom design for your tote bag")
