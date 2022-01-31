55d9e6b34b0df64661240190b8ad3fdb2fe15635
MarieCHessler <marie@hessler.se>
2022-01-31 12:35:30 +0000

    Add function for inner choice and validation of inner fabric, and change function for passing the data to the design worksheet

--- run.py
+++ run.py
@@ -70,12 +70,13 @@ def update_name_worksheet():
     print("Your name has been saved successfully :-)\n")
 
 
-def get_and_validate_outer_choice():
+def get_and_validate_outer_fabric():
     """
-    Outer fabric choice is collected, and validated in a
-    while True loop with a break statement.
+    Outer fabric choice is collected, and validated in a while True loop
+    with a break statement.
     The True loop makes sure the choice is in letters, and lower case.
-    If choice is missing, is not in letters, or is not cotton, linen or denim a ValueError is raised.
+    If choice is missing, is not in letters, or is not cotton,
+    linen or denim a ValueError is raised.
     """
     while True:
         choice_o_fabric = input("Please choose fabric (cotton, linen or denim): ").lower()
@@ -93,18 +94,43 @@ def get_and_validate_outer_choice():
     return choice_o_fabric
 
 
-def update_design_worksheet_outer():
+def get_and_validate_inner_fabric():
     """
-    Update the Google Sheets design worksheet with the collected choice of outer fabric.
+    Inner fabric choice is collected, and validated in a while True loop
+    with a break statement.
+    The True loop makes sure the choice is in letters, and lower case.
+    If choice is missing, is not in letters, or is not cotton,
+    linen or denim a ValueError is raised.
+    """
+    while True:
+        choice_i_fabric = input("Please choose fabric (cotton or spinnaker): ").lower()
+
+        try:
+            if choice_i_fabric.isalpha():
+                print(f"You chose {choice_i_fabric} for the inside of the bag. Great!\n")
+                break
+            else:
+                raise ValueError(f"You wrote {choice_i_fabric}, but we need a choice "
+                                 "between cotton and spinnaker, in letters please.")
+        except ValueError as e:
+            print(f"Invalid entry: {e}\n")  # Input from CI's Love Sandwiches
+
+    return choice_i_fabric
+
+
+def update_design_worksheet():
+    """
+    Update the Google Sheets design worksheet with the collected choices of fabric, colors and handles.
     """
     print("Your choice is being saved...\n")
     name_worksheet = SHEET.worksheet("design")  # Access design worksheet in Google Sheets
-    name_worksheet.append_row([outer_choice])  # The choice is saved in the design worksheet
+    name_worksheet.append_row([outer_fabric, inner_fabric])  # The choices are saved in the design worksheet
     print("Your choice has been saved successfully :-)\n")
 
 
 intro()
 new_name = get_and_validate_name()
 update_name_worksheet()
-outer_choice = get_and_validate_outer_choice()
-update_design_worksheet_outer()
+outer_fabric = get_and_validate_outer_fabric()
+inner_fabric = get_and_validate_inner_fabric()
+update_design_worksheet()
