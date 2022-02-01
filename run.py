fbd18b49babbffa80f30dc26aa6fa974046b96f8
MarieCHessler <marie@hessler.se>
2022-02-01 10:26:13 +0000

    Add function to collect and validate choices for handles, and update responses to the user and add printed bag in existing functions

--- run.py
+++ run.py
@@ -118,14 +118,38 @@ def get_and_validate_inner_fabric():
     return i_fabric
 
 
+def get_and_validate_handles_fabric():
+    """
+    Handles fabric choice is collected, and validated in a while True loop
+    with a break statement.
+    The True loop makes sure the choice is in letters, and lower case.
+    If choice is missing, is not in letters, or is not cotton or belt
+    a ValueError is raised.
+    """
+    while True:
+        h_fabric = input("Please choose fabric (cotton or belt): ").lower()
+
+        try:
+            if h_fabric.isalpha():
+                print(f"You chose {h_fabric} for the handles. Great!\n")
+                break
+            else:
+                raise ValueError(f"You wrote {h_fabric}, but we need a choice "
+                                 "between cotton and belt, in letters please.")
+        except ValueError as e:
+            print(f"Invalid entry: {e}\n")  # Input from CI's Love Sandwiches
+
+    return h_fabric
+
+
 def update_design_worksheet():
     """
     Update the Google Sheets design worksheet with the collected choices of fabrics, colors and handles.
     """
-    print("Your choice is being saved...\n")
+    print("Your choices are being saved...\n")
     design_worksheet = SHEET.worksheet("design")  # Access design worksheet in Google Sheets
-    design_worksheet.append_row([outer_fabric, inner_fabric])  # The choices are saved in the design worksheet
-    print("Your choice has been saved successfully :-)\n")
+    design_worksheet.append_row([outer_fabric, inner_fabric, handles_fabric])  # The choices are saved in the design worksheet
+    print("Your choices have been saved successfully :-)\n")
 
 
 def get_data_from_worksheets():
@@ -133,17 +157,28 @@ def get_data_from_worksheets():
     Get the data back from the Google Sheets name and design worksheets to thank the user
     for creating a bag with a selection of fabrics, colors and handles.
     """
-    print("Your bag is being collected...\n")
+    print("Your bag is being designed...\n")
     your_name = SHEET.worksheet("name").get_all_values()
     your_name_row = your_name[-1]
     choices = SHEET.worksheet("design").get_all_values()
     choices_row = choices[-1]
-    print(f"{your_name_row}, you have created a beautiful bag from {choices_row} :-)\n")
+    print(f"{your_name_row}, you have created a beautiful bag from {choices_row} \n\n")
+    print("     _______      ")
+    print("     |     |      ")
+    print("   -----------    ")
+    print("  |           |   ")
+    print("  |           |   ")
+    print("  |           |   ")
+    print("  |           |   ")
+    print("   -----------    \n\n")
+    print("Thank you for designing your bag with us!\n")
+
 
 intro()
 new_name = get_and_validate_name()
 update_name_worksheet()
 outer_fabric = get_and_validate_outer_fabric()
 inner_fabric = get_and_validate_inner_fabric()
+handles_fabric = get_and_validate_handles_fabric()
 update_design_worksheet()
 get_data_from_worksheets()
