22afe85f3f7c5736b1de46cbcc51d8df8e024ba5
MarieCHessler <marie@hessler.se>
2022-02-16 09:46:23 +0000

    Move functions to validations.py and remove an incorrect s from handles

--- run.py
+++ run.py
@@ -9,13 +9,14 @@ import gspread
 from google.oauth2.service_account import Credentials
 from colorama import init
 from termcolor import colored
-from constants import NEW_DESIGN, EXISTING_DESIGN, OUTER_FABRIC_OPTIONS
-from constants import OUTER_COLOR_OPTIONS, INNER_FABRIC_OPTIONS
-from constants import INNER_COLOR_OPTIONS, HANDLE_FABRIC_OPTIONS
-from constants import HANDLE_COLOR_OPTIONS
-from validations import new_or_existing_design, get_and_validate_inner_color
-from validations import input_and_validate_first_name, input_and_validate_last_name
-from validations import 
+from validations import new_or_existing_design, input_and_validate_first_name
+from validations import input_and_validate_last_name
+from validations import input_and_validate_outer_fabric
+from validations import input_and_validate_outer_color
+from validations import input_and_validate_inner_fabric
+from validations import input_and_validate_inner_color
+from validations import input_and_validate_handle_fabric
+from validations import input_and_validate_handle_color
 
 init()
 
@@ -63,15 +64,6 @@ def intro():
     time.sleep(7)
 
 
-
-
-
-
-
-
-
-
-
 def update_name_worksheet(full_name):
     """
     Update the Google Sheets name worksheet with the full name, using
@@ -85,181 +77,6 @@ def update_name_worksheet(full_name):
     name_worksheet.append_row([full_name])
 
     print("Now your name is in place. Thanks!\n\n")
-
-
-def input_and_validate_outer_fabric():
-    """
-    Collect and validate outer fabric in a while True loop with a
-    break statement.
-    Make sure, using the True loop, the choice is the correct word, in letters,
-    and lower case.
-    Raise ValueError if choice is missing, is not in letters, or is not cotton,
-    linen or denim.
-    """
-    while True:
-        # Have user enter fabric.
-        outer_fabric = input(colored("Would you like the outer fabric to be "
-                                     "cotton, linen or denim?\n",
-                                     "cyan")).lower()
-
-        try:
-            # Make sure the text is in letters and one of the right fabrics.
-            if outer_fabric.isalpha() and outer_fabric in OUTER_FABRIC_OPTIONS:
-                print(colored(f"\nYou chose {outer_fabric} for the outside. "
-                              "Nice!\n", "green"))
-                break
-            if not outer_fabric:
-                # State that no input has been made.
-                print("\nYou forgot to make a choice.\n")
-            else:
-                # State that the input is incorrect.
-                raise ValueError(f"\nYou wrote {outer_fabric}, but we need a "
-                                 "choice between cotton, linen and denim, "
-                                 "in letters please.\n")
-        except ValueError as e:
-            print(f"{e}\n")
-
-    return outer_fabric
-
-
-def input_and_validate_outer_color():
-    """
-    Collect and validate outer color in a while True loop with a
-    break statement.
-    Make sure, using the True loop, the choice is the correct word, in letters,
-    and lower case.
-    Raise ValueError if choice is missing, is not in letters, or is not blue,
-    cream, pink or grey.
-    """
-    while True:
-        # Have user enter color.
-        outer_color = input(colored("Do you prefer the outer color to be "
-                                    "blue, cream, pink or grey? \n",
-                                    "cyan")).lower()
-
-        try:
-            # Make sure the text is in letters and one of the right colors.
-            if outer_color.isalpha() and outer_color in OUTER_COLOR_OPTIONS:
-                print(colored(f"\nYou chose {outer_color} for the outside. "
-                              "Looks good!\n", "green"))
-                break
-            if not outer_color:
-                # State that no input has been made.
-                print("\nYou forgot to make a choice.\n")
-            else:
-                # State that the input is incorrect.
-                raise ValueError(f"\nYou wrote {outer_color}, but we need a "
-                                 "choice between blue, cream, pink and grey, "
-                                 "in letters please.\n")
-        except ValueError as e:
-            print(f"{e}\n")
-
-    return outer_color
-
-
-def input_and_validate_inner_fabric():
-    """
-    Collect and validate inner fabric in a while True loop with a
-    break statement.
-    Make sure, using the True loop, the choice is the correct word, in letters,
-    and lower case.
-    Raise ValueError if choice is missing, is not in letters, or is not cotton
-    or spinnaker.
-    """
-    while True:
-        # Have user enter fabric.
-        inner_fabric = input(colored("What kind of inner fabric do you "
-                                     "prefer, cotton or spinnaker?\n",
-                                     "cyan")).lower()
-
-        try:
-            # Make sure the text is in letters and one of the right fabrics.
-            if inner_fabric.isalpha() and inner_fabric in INNER_FABRIC_OPTIONS:
-                print(colored(f"\nYou chose {inner_fabric} for the inside. "
-                              "Good choice!\n", "green"))
-                break
-            if not inner_fabric:
-                # State that no input has been made.
-                print("\nYou forgot to make a choice.\n")
-            else:
-                # State that the input is incorrect.
-                raise ValueError(f"\nYou wrote {inner_fabric}, but we need a "
-                                 "choice between cotton and spinnaker, "
-                                 "in letters please.\n")
-        except ValueError as e:
-            print(f"{e}\n")
-
-    return inner_fabric
-
-
-def input_and_validate_handle_fabric():
-    """
-    Collect and validate handle fabric in a while True loop with a
-    break statement.
-    Make sure, using the True loop, the choice is the correct word, in letters,
-    and lower case.
-    Raise ValueError if choice is missing, is not in letters, or is not cotton,
-    or belt.
-    """
-    while True:
-        # Have user enter fabric.
-        handle_fabric = input(colored("For the handles, would you like them "
-                                      "to be made from cotton or belt? \n",
-                                      "cyan")).lower()
-
-        try:
-            # Make sure the text is in letters and one of the right fabrics.
-            if handle_fabric.isalpha() and handle_fabric in HANDLE_FABRIC_OPTIONS:
-                print(colored(f"\nYou chose {handle_fabric} for the handles. "
-                              "Great!\n", "green"))
-                break
-            if not handle_fabric:
-                # State that no input has been made.
-                print("\nYou forgot to make a choice.\n")
-            else:
-                # State that the input is incorrect.
-                raise ValueError(f"\nYou wrote {handle_fabric}, but we need a "
-                                 "choice between cotton and belt, "
-                                 "in letters please.\n")
-        except ValueError as e:
-            print(f"{e}\n")
-
-    return handle_fabric
-
-
-def input_and_validate_handle_color():
-    """
-    Collect and validate handle color in a while True loop with a
-    break statement.
-    Make sure, using the True loop, the choice is the correct word, in letters,
-    and lower case.
-    Raise ValueError if choice is missing, is not in letters, or is not blue,
-    white or grey.
-    """
-    while True:
-        # Have user enter color.
-        handle_color = input(colored("Do you prefer the handle color to be "
-                                     "blue, white or grey? \n",
-                                     "cyan")).lower()
-
-        try:
-            # Make sure the text is in letters and one of the right colors.
-            if handle_color.isalpha() and handle_color in HANDLE_COLOR_OPTIONS:
-                print(colored(f"\nYou chose {handle_color} for the handles. "
-                              "Stylish!\n", "green"))
-                break
-            if not handle_color:
-                # State that no input has been made.
-                print("\nYou forgot to make a choice.\n")
-            else:
-                # State that the input is incorrect.
-                raise ValueError(f"\nYou wrote {handle_color}, but we need a "
-                                 "choice between blue, white and grey, "
-                                 "in letters please.\n")
-        except ValueError as e:
-            print(f"{e}\n")
-
-    return handle_color
 
 
 def update_design_worksheet(outer_color, outer_fabric, inner_color,
@@ -481,10 +298,10 @@ def main():
     outer_color = input_and_validate_outer_color()
     inner_fabric = input_and_validate_inner_fabric()
     inner_color = input_and_validate_inner_color()
-    handles_fabric = input_and_validate_handle_fabric()
-    handles_color = input_and_validate_handle_color()
-    update_design_worksheet(o_color, o_fabric, i_color, i_fabric,
-                            h_color, h_fabric)
+    handle_fabric = input_and_validate_handle_fabric()
+    handle_color = input_and_validate_handle_color()
+    update_design_worksheet(outer_color, outer_fabric, inner_color,
+                            inner_fabric, handle_color, handle_fabric)
     present_no = get_design_no_from_worksheet()
     return_new_no_to_worksheet(present_no)
     create_unique_id()
--- validations.py
+++ validations.py
@@ -1,5 +1,12 @@
+"""
+Validate to make sure user input is correct
+"""
+import time
 from termcolor import colored
-from constants import INNER_COLOR_OPTIONS
+from constants import NEW_DESIGN, EXISTING_DESIGN, OUTER_FABRIC_OPTIONS
+from constants import OUTER_COLOR_OPTIONS, INNER_FABRIC_OPTIONS
+from constants import INNER_COLOR_OPTIONS, HANDLE_FABRIC_OPTIONS
+from constants import HANDLE_COLOR_OPTIONS
 # from run import find_bag_design
 
 
@@ -99,6 +106,111 @@ def input_and_validate_last_name(first_name):
     return full_name
 
 
+def input_and_validate_outer_fabric():
+    """
+    Collect and validate outer fabric in a while True loop with a
+    break statement.
+    Make sure, using the True loop, the choice is the correct word, in letters,
+    and lower case.
+    Raise ValueError if choice is missing, is not in letters, or is not cotton,
+    linen or denim.
+    """
+    while True:
+        # Have user enter fabric.
+        outer_fabric = input(colored("Would you like the outer fabric to be "
+                                     "cotton, linen or denim?\n",
+                                     "cyan")).lower()
+
+        try:
+            # Make sure the text is in letters and one of the right fabrics.
+            if outer_fabric.isalpha() and outer_fabric in OUTER_FABRIC_OPTIONS:
+                print(colored(f"\nYou chose {outer_fabric} for the outside. "
+                              "Nice!\n", "green"))
+                break
+            if not outer_fabric:
+                # State that no input has been made.
+                print("\nYou forgot to make a choice.\n")
+            else:
+                # State that the input is incorrect.
+                raise ValueError(f"\nYou wrote {outer_fabric}, but we need a "
+                                 "choice between cotton, linen and denim, "
+                                 "in letters please.\n")
+        except ValueError as e:
+            print(f"{e}\n")
+
+    return outer_fabric
+
+
+def input_and_validate_outer_color():
+    """
+    Collect and validate outer color in a while True loop with a
+    break statement.
+    Make sure, using the True loop, the choice is the correct word, in letters,
+    and lower case.
+    Raise ValueError if choice is missing, is not in letters, or is not blue,
+    cream, pink or grey.
+    """
+    while True:
+        # Have user enter color.
+        outer_color = input(colored("Do you prefer the outer color to be "
+                                    "blue, cream, pink or grey? \n",
+                                    "cyan")).lower()
+
+        try:
+            # Make sure the text is in letters and one of the right colors.
+            if outer_color.isalpha() and outer_color in OUTER_COLOR_OPTIONS:
+                print(colored(f"\nYou chose {outer_color} for the outside. "
+                              "Looks good!\n", "green"))
+                break
+            if not outer_color:
+                # State that no input has been made.
+                print("\nYou forgot to make a choice.\n")
+            else:
+                # State that the input is incorrect.
+                raise ValueError(f"\nYou wrote {outer_color}, but we need a "
+                                 "choice between blue, cream, pink and grey, "
+                                 "in letters please.\n")
+        except ValueError as e:
+            print(f"{e}\n")
+
+    return outer_color
+
+
+def input_and_validate_inner_fabric():
+    """
+    Collect and validate inner fabric in a while True loop with a
+    break statement.
+    Make sure, using the True loop, the choice is the correct word, in letters,
+    and lower case.
+    Raise ValueError if choice is missing, is not in letters, or is not cotton
+    or spinnaker.
+    """
+    while True:
+        # Have user enter fabric.
+        inner_fabric = input(colored("What kind of inner fabric do you "
+                                     "prefer, cotton or spinnaker?\n",
+                                     "cyan")).lower()
+
+        try:
+            # Make sure the text is in letters and one of the right fabrics.
+            if inner_fabric.isalpha() and inner_fabric in INNER_FABRIC_OPTIONS:
+                print(colored(f"\nYou chose {inner_fabric} for the inside. "
+                              "Good choice!\n", "green"))
+                break
+            if not inner_fabric:
+                # State that no input has been made.
+                print("\nYou forgot to make a choice.\n")
+            else:
+                # State that the input is incorrect.
+                raise ValueError(f"\nYou wrote {inner_fabric}, but we need a "
+                                 "choice between cotton and spinnaker, "
+                                 "in letters please.\n")
+        except ValueError as e:
+            print(f"{e}\n")
+
+    return inner_fabric
+
+
 def input_and_validate_inner_color():
     """
     Collect and validate inner color in a while True loop with a
@@ -132,3 +244,73 @@ def input_and_validate_inner_color():
             print(f"{e}\n")
 
     return inner_color
+
+
+def input_and_validate_handle_fabric():
+    """
+    Collect and validate handle fabric in a while True loop with a
+    break statement.
+    Make sure, using the True loop, the choice is the correct word, in letters,
+    and lower case.
+    Raise ValueError if choice is missing, is not in letters, or is not cotton,
+    or belt.
+    """
+    while True:
+        # Have user enter fabric.
+        handle_fabric = input(colored("For the handles, would you like them "
+                                      "to be made from cotton or belt? \n",
+                                      "cyan")).lower()
+
+        try:
+            # Make sure the text is in letters and one of the right fabrics.
+            if handle_fabric.isalpha() and handle_fabric in HANDLE_FABRIC_OPTIONS:
+                print(colored(f"\nYou chose {handle_fabric} for the handles. "
+                              "Great!\n", "green"))
+                break
+            if not handle_fabric:
+                # State that no input has been made.
+                print("\nYou forgot to make a choice.\n")
+            else:
+                # State that the input is incorrect.
+                raise ValueError(f"\nYou wrote {handle_fabric}, but we need a "
+                                 "choice between cotton and belt, "
+                                 "in letters please.\n")
+        except ValueError as e:
+            print(f"{e}\n")
+
+    return handle_fabric
+
+
+def input_and_validate_handle_color():
+    """
+    Collect and validate handle color in a while True loop with a
+    break statement.
+    Make sure, using the True loop, the choice is the correct word, in letters,
+    and lower case.
+    Raise ValueError if choice is missing, is not in letters, or is not blue,
+    white or grey.
+    """
+    while True:
+        # Have user enter color.
+        handle_color = input(colored("Do you prefer the handle color to be "
+                                     "blue, white or grey? \n",
+                                     "cyan")).lower()
+
+        try:
+            # Make sure the text is in letters and one of the right colors.
+            if handle_color.isalpha() and handle_color in HANDLE_COLOR_OPTIONS:
+                print(colored(f"\nYou chose {handle_color} for the handles. "
+                              "Stylish!\n", "green"))
+                break
+            if not handle_color:
+                # State that no input has been made.
+                print("\nYou forgot to make a choice.\n")
+            else:
+                # State that the input is incorrect.
+                raise ValueError(f"\nYou wrote {handle_color}, but we need a "
+                                 "choice between blue, white and grey, "
+                                 "in letters please.\n")
+        except ValueError as e:
+            print(f"{e}\n")
+
+    return handle_color
