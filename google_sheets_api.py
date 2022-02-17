be9bc0fad0effad23ab28dce2ec7b91bdc8ce07f
MarieCHessler <marie@hessler.se>
2022-02-17 14:32:40 +0000

    Adjust printed text for better fit to window

--- google_sheets_api.py
+++ google_sheets_api.py
@@ -33,14 +33,14 @@ def update_name_worksheet(full_name):
     Update the Google Sheets name worksheet with the full name, using
     the append() method.
     """
-    print("Your name is being saved...\n")
+    print("  Your name is being saved.\n")
 
     # Access Google Sheets worksheet.
     name_worksheet = SHEET.worksheet("name")
     # Save the full name in the worksheet.
     name_worksheet.append_row([full_name])
 
-    print("Now your name is in place. Thanks!\n\n")
+    print("  Now your name is in place. Thanks!\n\n")
 
 
 def update_design_worksheet(outer_color, outer_fabric, inner_color,
@@ -49,7 +49,7 @@ def update_design_worksheet(outer_color, outer_fabric, inner_color,
     Update the Google Sheets design worksheet with the collected choices of
     fabrics, colors and handles using the append() method.
     """
-    print("\nYour design choices are being saved...\n")
+    print("\n  Your design choices are being saved.\n")
 
     # Access Google Sheets worksheet.
     design_worksheet = SHEET.worksheet("design")
@@ -57,7 +57,7 @@ def update_design_worksheet(outer_color, outer_fabric, inner_color,
     design_worksheet.append_row([outer_color, outer_fabric, inner_color,
                                  inner_fabric, handle_color, handle_fabric])
 
-    print("Your choices have been saved successfully.\n")
+    print("  Your choices have been saved successfully.\n")
 
 
 def get_design_no_from_worksheet():
@@ -81,14 +81,14 @@ def return_new_no_to_worksheet(present_no):
     new_no = present_no  # Create new design number variable from present.
     new_no = int(new_no) + 1  # Increment the new design number by one.
 
-    print("A design number is being created...\n")
+    print("  A design number is being created.\n")
 
     # Access Google Sheets worksheet.
     design_no_worksheet = SHEET.worksheet("design_no")
     # Save the number in the design number worksheet.
     design_no_worksheet.append_row([new_no])
 
-    print("The number has been saved successfully.\n")
+    print("  The number has been saved successfully.\n")
 
 
 def create_unique_id():
@@ -110,14 +110,14 @@ def create_unique_id():
     # Create unique ID from name and design number.
     unique_new_id = youridname + design_no
 
-    print("Your unique design ID is being created...\n")
+    print("  Your unique design ID is being created.\n")
 
     # Access Google Sheets worksheet.
     unique_id_worksheet = SHEET.worksheet("unique_id")
     # Save the unique ID in the unique ID worksheet.
     unique_id_worksheet.append_row([unique_new_id])
 
-    print("Your unique design ID has been saved successfully\n\n")
+    print("  Your unique design ID has been saved successfully.\n\n")
     time.sleep(3)
 
 
@@ -129,7 +129,7 @@ def get_data_from_worksheets():
     Sum up the tote bag design, by showing the selection of fabrics, colors
     and handles, and the design ID to the user in a print statement.
     """
-    print("Your tote bag is now being designed...\n\n")
+    print("  Your tote bag is now being designed :-)\n\n")
     time.sleep(3)
 
     # Access Google Sheets worksheet to get all values in worksheet.
@@ -154,14 +154,14 @@ def get_data_from_worksheets():
     unique_id_row = all_unique_ids[-1]  # Slice final item from the list.
     unique_id = unique_id_row[0]  # Select the first item from the row.
 
-    print(colored(f"You are a great designer {user_name}!\n", "green"))
-    print(colored(f"Your own cool tote bag is made from an outside of "
+    print(colored(f"  You are a great designer {user_name}!\n", "green"))
+    print(colored(f"  Your own cool tote bag is made from an outside of "
                   f"{outer_choice},", "green"))
-    print(colored(f"an inside of {inner_choice}, and {handle_choice} handles.",
-                  "green"))
-    print(colored(f"The unique ID for your design is {unique_id}, and you can "
-                  "use it to revisit", "green"))
-    print(colored("your design.\n", "green"))
+    print(colored(f"  an inside of {inner_choice}, and {handle_choice} "
+                  "handles.", "green"))
+    print(colored(f"  The unique ID for your design is {unique_id}, and you "
+                  "can use it to revisit", "green"))
+    print(colored("  your design.\n", "green"))
 
     print("""
         ____
@@ -171,16 +171,17 @@ def get_data_from_worksheets():
       | Tote |
        ------   \n
     """)
+    time.sleep(6)
+
+    print(colored("  If you want to design a new tote bag, you can do so "
+                  "shortly, when you have", "blue"))
+    print(colored("  been returned to the start page.\n", "blue"))
+
+    print(colored("  Thank you for designing your bag with us!\n\n", "blue"))
     time.sleep(5)
 
-    print(colored("If you want to design a new tote bag, you can do so "
-                  "shortly, when you have", "blue"))
-    print(colored("been returned to the start page.\n", "blue"))
-
-    print(colored("Thank you for designing your bag with us!\n\n", "blue"))
-    time.sleep(4)
-
-    print(colored("You will now be returned to the start page.\n\n", "blue"))
+    print(colored("  We will now take you back to the start page.\n\n",
+                  "blue"))
     time.sleep(3)
 
     os.system("python run.py")
